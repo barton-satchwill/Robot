399d6cc60938bf8be11cb14d061d334d019beeb8
Barton Satchwill <barton.satchwill@gmail.com>
2014-02-09 10:56:31 -0700

    add libraries

---
+++ lib/Robot.h
@@ -0,0 +1,48 @@
+/*
+  Robot.h -
+  Released into the public domain.
+*/
+
+#ifndef Robot_h
+#define Robot_h
+
+#include <Arduino.h>
+#include <AFMotor.h>
+#include <Motor.h>
+#include <Sonar.h>
+#include <lightSensor.h>
+#include <Servo.h>
+
+
+class Robot
+{
+public:
+    Robot(int speed);
+    void setSpeed(int theSpeed);
+    String toString();
+    void forward(long time);
+    void backward(long time);
+    void halt(long time);
+    void turnLeft(int degrees);
+    void turnRight(int degrees);
+    void rangeScan(int scanAngle);
+    float range();
+    float rangeAhead();
+    int getTurnAngle();
+    void clearTurnAngle();
+    void initiateRangeScan();
+
+    void moveServo(int angle);
+
+private:
+    boolean servoReady();
+    boolean scanComplete();
+    int id;
+    static int count;
+    int speed;
+    Motor motor;
+    Sonar sonar;
+    // LightSensor lightSensor;
+};
+
+#endif
